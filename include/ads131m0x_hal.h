8769ced9aab4429ad7bbde6d76195b8216a1e22a
Lan <lan@crablabs.io>
2026-03-04 11:26:17 -0800

    Not use resetSet for now

--- include/ads131m0x_hal.h
+++ include/ads131m0x_hal.h
@@ -10,7 +10,7 @@ typedef struct
 {
 	int (*spiRead)(void* const data, const uint8_t length);
 	int (*spiWrite)(const void* const data, const uint8_t length);
-	void (*resetSet)(const bool state);       ///< Active-low hardware reset
+	// void (*resetSet)(const bool state);       ///< Active-low hardware reset
 	void (*sleepSet)(const bool state);       ///< Sleep/enable control
 	bool (*drdyGet)(void);                    ///< Data ready interrupt pin
 	void (*delayMs)(const uint32_t delayMs);
